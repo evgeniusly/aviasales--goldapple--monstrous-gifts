03aaa35eb8c71b9106e38b541fe2cd9281b475de
evgeniusly <pak.evgeny@live.ru>
2026-02-10 14:49:44 +0300

    feat: add preloads

--- apps/general/src/data/preloads.tsx
+++ apps/general/src/data/preloads.tsx
@@ -1,18 +1,31 @@
-import aviasalesLogo from '~/assets/images/aviasalesLogo.svg'
-import clientLogo from '~/assets/images/clientLogo.svg'
+import aviasalesLogo from '@assets/images/aviasalesLogo.svg'
+import clientLogo from '@assets/images/clientLogo.svg'
+import gameParticlesClose from '@assets/images/gameParticlesClose.png'
+import gameParticlesFar from '@assets/images/gameParticlesFar.png'
+import gamePersonMob from '@assets/images/gamePersonMob.png'
+import homeGifts from '@assets/images/homeGifts.png'
+import homeParticlesClose from '@assets/images/homeParticlesClose.png'
+import homeParticlesFar from '@assets/images/homeParticlesFar.png'
+import homeParticlesMob from '@assets/images/homeParticlesMob.png'
+import homePersonBody from '@assets/images/homePersonBody.png'
+import homePersonHand from '@assets/images/homePersonHand.png'
+import loaderAnim from '@assets/images/loader.webp'
+import loaderParticles from '@assets/images/loaderParticles.png'
+import loaderParticlesMob from '@assets/images/loaderParticlesMob.png'
+import personIngame from '@assets/images/personIngame80.webp'
 
 export const preloads = {
   preApp: [aviasalesLogo, clientLogo],
   preAppDesk: [],
   preAppMob: [],
 
-  preHome: [],
-  preHomeDesk: [],
-  preHomeMob: [],
+  preHome: [homePersonBody, homePersonHand],
+  preHomeDesk: [homeParticlesClose, homeParticlesFar, homeGifts],
+  preHomeMob: [homeParticlesMob],
 
-  preGame: [],
-  preGameDesk: [],
-  preGameMob: [],
+  preGame: [loaderAnim],
+  preGameDesk: [gameParticlesClose, gameParticlesFar, loaderParticles, personIngame],
+  preGameMob: [gamePersonMob, loaderParticlesMob],
 
   inGame: [],
   inGameDesk: [],
