7a07a78a88ff0ee504f414faebc79cb4a8c5ad9c
evgeniusly <pak.evgeny@live.ru>
2026-02-10 15:06:45 +0300

    feat: update meta

--- apps/general/src/defs/consts/meta.const.ts
+++ apps/general/src/defs/consts/meta.const.ts
@@ -2,7 +2,7 @@ import { getPath } from '../../../defs/utils'
 
 export const meta = {
   url: getPath(),
-  siteName: 'siteName',
-  title: 'Title',
-  description: 'description',
+  siteName: 'Чудовищные подарки',
+  title: 'Что подарить близким?',
+  description: 'У нашей злюки есть только неправильные ответы. Ещё у неё есть 40 000 баллов на авиабилеты',
 }
